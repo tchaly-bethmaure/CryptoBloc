656919c0d99390263d05c1503a27747938b22b4a
tchaly <tchaly@TchalyDebian>
2017-02-05 17:36:01 +0100

    Crypt et decrypt

--- cryptoblock.py
+++ cryptoblock.py
@@ -1,26 +1,30 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import random
 import hashlib
 import math
+from Crypto.Cipher import AES
+import os
 
-# NOTE :
-# 1) il reste à développer le cipher et faire marcher le tout.
+def encryption(text, clef, iv):
+	mon_objet_d_encryption = AES.new(clef, AES.MODE_CBC, iv)
+	return mon_objet_d_encryption.encrypt(text)
 
-def xor(bloc_text, iv):
+def decryption(text, clef, iv):
+	mon_object_de_decryption = AES.new(clef, AES.MODE_CBC, iv)
+	return mon_object_de_decryption.decrypt(text)
+
+def xor(bloc_text, vecteur):
 	# On prépare notre encryption pour qu'elle soit faite octet par octet
 	tab_of_char = []
 	for c in bloc_text:
 		tab_of_char.append(c)
-	tab_of_key_char = []
-	for c in iv:
-		tab_of_key_char.append(c)
-	# On encrypte
+	# On xor
 	tab_encrypte = []
 	for i in range(len(tab_of_char)):
-		tab_encrypte.append(chr(ord(tab_of_char[i])^ord(tab_of_key_char[i])))
+		tab_encrypte.append(chr(ord(tab_of_char[i])^vecteur))
 	return ''.join(tab_encrypte)
-	
+
 def decoupage_par_bloc(text, taille_bloc):
 	if(taille_bloc > len(text)):
 		print("Le découpage n'est pas possible sans rajout de bloc fictif (option à rajouter si amélioration demandée).")
@@ -37,36 +41,25 @@ def decoupage_par_bloc(text, taille_bloc):
 			curseur_fin_bloc += taille_bloc
 		return bloc_de_text
 
+mon_text_a_encrypter = u"MonSuperTextSupraLongueMonSuperTextSupraLongueMonSuperTextSupraLongueMonSuperTextSupraLongueMonSuperTextSupraLongueMonSuperTextSupraLongueMonSuperTextSupraLongueMonSuperTextSupraLongueMonSuperTextSupraLongueMonSuperTextSupraLongueMonSuperTextSupraLongueMonSuperTextSupraLongueMonSuperTextSupraLongueMonSuperTextSupraLongueMonSuperTextSupraLongueMonSuperTextSupraLongueMonSuperTextSupraLongue"
+ma_clef_dencryption = os.urandom(32)
+ma_clef_de_decriptage = os.urandom(32)
+iv = os.urandom(16)
 
-def decoupage_par_bloc_de_n_bit(text, taille_bloc_en_bit):
-	newText = "";
-	for c in text:
-		newText += bin(ord(c))
-	text = newText
-	if(taille_bloc_en_bit > len(text)):
-		print("Le découpage n'est pas possible sans rajout de bloc fictif (option à rajouter si amélioration demandée).")
-		exit()
-	else:
-		bloc_de_text = []
-		nombre_de_bloc = int(math.floor(len(text)/taille_bloc_en_bit))
-		curseur_debut_bloc = 0
-		curseur_fin_bloc = taille_bloc_en_bit
-		for index_bloc in range(nombre_de_bloc):
-			bloc_de_text.append(text[curseur_debut_bloc:curseur_fin_bloc])
-			temp = curseur_fin_bloc
-			curseur_debut_bloc = curseur_fin_bloc
-			curseur_fin_bloc += taille_bloc_en_bit
-		return bloc_de_text
+print("############################# ENCRYPT ################################")
 
-mon_text_a_encrypter = u"MonSuperTextSupraLongavecdeséééééetdesààààà"
-#decoupage_par_bloc(mon_text_a_encrypter, 4)
-print(decoupage_par_bloc_de_n_bit(mon_text_a_encrypter, 8))
-# iv = random.getrandbits(2048)
+mes_blocs = decoupage_par_bloc(mon_text_a_encrypter, len(ma_clef_dencryption))
+mes_blocs_encryptes = []
+# On encrypte par bloc :
+for bloc in mes_blocs:
+	cipher_text = encryption(bloc, ma_clef_dencryption, iv)
+	mes_blocs_encryptes.append(cipher_text)
+print(''.join(mes_blocs_encryptes))
 
-# sha = hashlib.sha512()
-# sha.update(str(ma_super_clef))
-# ma_clef = sha.digest()
+print("############################# DECRYPT ################################")
 
-# text_crypte = encrypt_bloc(mon_text_a_encrypter, ma_clef)
-# text_decrypte = encrypt_bloc(text_crypte, ma_clef)
-# print(text_decrypte)
+mes_blocs_decryptes = []
+for bloc in mes_blocs_encryptes:
+	cipher_text = decryption(bloc, ma_clef_dencryption, iv)
+	mes_blocs_decryptes.append(cipher_text)
+print(''.join(mes_blocs_decryptes))
